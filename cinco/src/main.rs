66af936f79e4d7f5366f23ff68150fc1ea9385fa
Raymundo Vásquez Ruiz <raymundo.vr@protonmail.com>
2022-04-13 19:45:49 +0200

    dia doce

--- cinco/src/main.rs
+++ cinco/src/main.rs
@@ -1,4 +1,7 @@
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder, HttpResponseBuilder, http::StatusCode};
+use actix_web::{
+    delete, get, http::StatusCode, patch, post, web, App, HttpResponse, HttpResponseBuilder,
+    HttpServer, Responder,
+};
 use serde::{Deserialize, Serialize};
 use serde_json::json;
 use std::sync::Mutex;
@@ -52,6 +55,8 @@ async fn main() -> std::io::Result<()> {
             .service(get_albums)
             .service(post_album)
             .service(get_album)
+            .service(update_album)
+            .service(delete_album)
     })
     .bind(("127.0.0.1", 8080))?
     .run()
@@ -80,12 +85,62 @@ async fn post_album(data: web::Data<AppState>, body: String) -> HttpResponseBuil
 async fn get_album(data: web::Data<AppState>, path: web::Path<String>) -> impl Responder {
     let albums = data.albums.lock().unwrap();
     let id = path.into_inner();
-    println!("Received {}", id);
     for album in albums.iter() {
         if album.id == id {
-            return web::Json(json!(album)).customize().with_status(StatusCode::OK);
+            return web::Json(json!(album))
+                .customize()
+                .with_status(StatusCode::OK);
         }
     }
 
-    web::Json(json!("not found")).customize().with_status(StatusCode::NOT_FOUND)
+    web::Json(json!("not found"))
+        .customize()
+        .with_status(StatusCode::NOT_FOUND)
+}
+
+#[patch("/albums/{id}")]
+async fn update_album(
+    data: web::Data<AppState>,
+    path: web::Path<String>,
+    body: String,
+) -> impl Responder {
+    let mut albums = data.albums.lock().unwrap();
+    let id = path.into_inner();
+    let payload: Result<Album, serde_json::Error> = serde_json::from_str(&body);
+
+    if let Ok(album) = payload {
+        match albums.iter().position(|a| a.id == id) {
+            Some(i) => {
+                albums[i] = album;
+                return web::Json(json!(albums[i]))
+                    .customize()
+                    .with_status(StatusCode::OK);
+            }
+            None => {
+                return web::Json(json!("not found"))
+                    .customize()
+                    .with_status(StatusCode::NOT_FOUND);
+            }
+        }
+    }
+
+    web::Json(json!("{error: invalid payload}"))
+        .customize()
+        .with_status(StatusCode::BAD_REQUEST)
+}
+
+#[delete("/albums/{id}")]
+async fn delete_album(data: web::Data<AppState>, path: web::Path<String>) -> HttpResponseBuilder {
+    let mut albums = data.albums.lock().unwrap();
+    let id = path.into_inner();
+
+    match albums.iter().position(|a| a.id == id) {
+        Some(i) => {
+            albums.remove(i);
+            HttpResponse::Ok()
+        }
+        None => {
+            HttpResponse::NotFound()
+        }
+    }
 }
