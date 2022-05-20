24d656aad4499d1e947f6c3a6a72157b199fe448
Raymundo Vásquez Ruiz <raymundo.vr@protonmail.com>
2022-05-20 22:44:51 +0200

    dia ventiseis

--- auth/src/main.rs
+++ auth/src/main.rs
@@ -1,9 +1,7 @@
-use actix_web::{App, get, HttpResponse, HttpServer, Responder};
+mod general;
 
-#[get("/health")]
-async fn health_check() -> impl Responder {
-    HttpResponse::Ok().body("ok")
-}
+use actix_web::{App, HttpServer};
+use general::health_check;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
@@ -14,24 +12,4 @@ async fn main() -> std::io::Result<()> {
     .bind(("127.0.0.1", 8080))?
     .run()
     .await
-}
-
-#[cfg(test)]
-mod test {
-    use super::*;
-    use actix_web::
-        {
-            App,
-            http::{self},
-            test,
-        };
-
-    #[actix_web::test]
-    async fn test_health() {
-        let app = test::init_service(App::new().service(health_check)).await;
-        let request = test::TestRequest::get().uri("/health").to_request();
-        let response = test::call_service(&app, request).await;
-        println!("{:?}", response);
-        assert_eq!(response.status(), http::StatusCode::OK);
-    }
 }
