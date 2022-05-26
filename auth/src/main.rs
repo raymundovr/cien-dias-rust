1c1417ff16820d8529db9935fb57aa95a3579375
Raymundo Vásquez Ruiz <raymundo.vr@protonmail.com>
2022-05-26 20:54:46 +0200

    31: refactor para reutilizar app

--- auth/src/main.rs
+++ auth/src/main.rs
@@ -1,27 +1,13 @@
 mod auth;
 mod general;
 mod token;
+mod app;
 
-use actix_web::{cookie::Key, middleware::Logger, App, HttpServer};
-use actix_session::{SessionMiddleware, storage::CookieSessionStore};
-use auth::*;
+use app::launch_server;
 use env_logger::Env;
-use general::health_check;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     env_logger::init_from_env(Env::default().default_filter_or("info"));
-    let session_key = Key::generate();
-
-    HttpServer::new(move || {
-        App::new()
-            .wrap(Logger::default())
-            .wrap(SessionMiddleware::new(CookieSessionStore::default(), session_key.clone()))
-            .service(health_check)
-            .service(login_token)
-            .service(login_session)
-    })
-    .bind(("127.0.0.1", 8080))?
-    .run()
-    .await
+    launch_server()?.await
 }
