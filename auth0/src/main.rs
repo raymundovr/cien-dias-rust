3f9ad3ba27135ad515cb8ef27707dfd216c398eb
Raymundo Vásquez Ruiz <raymundo.vr@protonmail.com>
2022-06-05 19:51:54 +0200

    38: anyhow y actix-web

--- auth0/src/main.rs
+++ auth0/src/main.rs
@@ -14,7 +14,7 @@ use auth0::{
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let jwks_uri = env::var("JWKS_URI").expect("Must set JWKS_URI");
-    let token = r#"eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IllzZ0JtNWk2TDhIRjFFbjZXV2gwSCJ9.eyJpc3MiOiJodHRwczovL3Zhc3F1ZXpydWl6LmV1LmF1dGgwLmNvbS8iLCJzdWIiOiJVOHhzcEplUEtNQjhabUpoM2ZjZDNIM0M1ZlptMUh1ZkBjbGllbnRzIiwiYXVkIjoiaHR0cHM6Ly9zYWx1ZGFyaW8udmFzcXVlenJ1aXouY29tIiwiaWF0IjoxNjU0Mjg1ODcwLCJleHAiOjE2NTQzNzIyNzAsImF6cCI6IlU4eHNwSmVQS01COFptSmgzZmNkM0gzQzVmWm0xSHVmIiwiZ3R5IjoiY2xpZW50LWNyZWRlbnRpYWxzIn0.DxJmn6AHb1x_paJxkk405MKv42Lt87_kobafzceV3KrPSbVaP3sEjSMPKII6EAeftF1-qcOgh9EngMLVYb7_gOzb1T2PFXq9mmd1SYSYdk0jfG8acQ1t__L1IqOxcV3chX3aUuERL5yWEglEOKV43_qtCihMHhU51GTr_oEuebZ7auuT6A-mVDjJdHefaT6EZx8af_307nfeik-cRij4sFpkL-ZIntP3E_wvcu9voCP-wL9fzIkQEuE32CIxGgBoNNImjZ1prqjH41Rg-IfuHLvAJpqkmiB2E5KoU9LEspXn1gpC8DC1v1cW7XfydzzcXpUOqHMuQgAa_LEy5fwcag"#;
+    let token = r#"some-token-here";
     println!("{:?}", jwks_uri);
 
     let mut auth0_validation = Auth0Validation::new(
