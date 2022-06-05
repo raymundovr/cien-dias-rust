4a10563c92bbc8c4aa1f0beef97c3127d15b942e
Raymundo Vásquez Ruiz <raymundo.vr@protonmail.com>
2022-06-05 12:03:47 +0200

    38: auth0

--- auth0/src/main.rs
+++ auth0/src/main.rs
@@ -1,16 +1,36 @@
-use jsonwebtoken::{jwk::{AlgorithmParameters, JwkSet}, decode, decode_header, Validation, DecodingKey};
-use reqwest::get;
+mod auth0;
+
 use std::env;
-use std::collections::HashMap;
+use auth0::{
+    Auth0Validation,
+    Claims
+};
+
+/*
+    audience: 'https://saludario.vasquezruiz.com',
+    issuer: 'https://vasquezruiz.eu.auth0.com/',
+*/
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let jwks_uri = env::var("JWKS_URI").expect("Must set JWKS_URI");
+    let token = r#"eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IllzZ0JtNWk2TDhIRjFFbjZXV2gwSCJ9.eyJpc3MiOiJodHRwczovL3Zhc3F1ZXpydWl6LmV1LmF1dGgwLmNvbS8iLCJzdWIiOiJVOHhzcEplUEtNQjhabUpoM2ZjZDNIM0M1ZlptMUh1ZkBjbGllbnRzIiwiYXVkIjoiaHR0cHM6Ly9zYWx1ZGFyaW8udmFzcXVlenJ1aXouY29tIiwiaWF0IjoxNjU0Mjg1ODcwLCJleHAiOjE2NTQzNzIyNzAsImF6cCI6IlU4eHNwSmVQS01COFptSmgzZmNkM0gzQzVmWm0xSHVmIiwiZ3R5IjoiY2xpZW50LWNyZWRlbnRpYWxzIn0.DxJmn6AHb1x_paJxkk405MKv42Lt87_kobafzceV3KrPSbVaP3sEjSMPKII6EAeftF1-qcOgh9EngMLVYb7_gOzb1T2PFXq9mmd1SYSYdk0jfG8acQ1t__L1IqOxcV3chX3aUuERL5yWEglEOKV43_qtCihMHhU51GTr_oEuebZ7auuT6A-mVDjJdHefaT6EZx8af_307nfeik-cRij4sFpkL-ZIntP3E_wvcu9voCP-wL9fzIkQEuE32CIxGgBoNNImjZ1prqjH41Rg-IfuHLvAJpqkmiB2E5KoU9LEspXn1gpC8DC1v1cW7XfydzzcXpUOqHMuQgAa_LEy5fwcag"#;
     println!("{:?}", jwks_uri);
-    let jwks_resp = get(&jwks_uri).await?.text().await?;
+
+    let mut auth0_validation = Auth0Validation::new(
+        jwks_uri,
+        Claims {
+            aud: "https://saludario.vasquezruiz.com".to_string(),
+            iss: "https://vasquezruiz.eu.auth0.com/".to_string(),
+        }
+    );
+
+    println!("Is valid {:?}", auth0_validation.is_token_valid(&token).await);
+
+    /* let jwks_resp = get(&jwks_uri).await?.text().await?;
     let jwks: JwkSet = serde_json::from_str(&jwks_resp)?;
     
-    let token = r#"eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IllzZ0JtNWk2TDhIRjFFbjZXV2gwSCJ9.eyJpc3MiOiJodHRwczovL3Zhc3F1ZXpydWl6LmV1LmF1dGgwLmNvbS8iLCJzdWIiOiJVOHhzcEplUEtNQjhabUpoM2ZjZDNIM0M1ZlptMUh1ZkBjbGllbnRzIiwiYXVkIjoiaHR0cHM6Ly9zYWx1ZGFyaW8udmFzcXVlenJ1aXouY29tIiwiaWF0IjoxNjU0Mjg1ODcwLCJleHAiOjE2NTQzNzIyNzAsImF6cCI6IlU4eHNwSmVQS01COFptSmgzZmNkM0gzQzVmWm0xSHVmIiwiZ3R5IjoiY2xpZW50LWNyZWRlbnRpYWxzIn0.DxJmn6AHb1x_paJxkk405MKv42Lt87_kobafzceV3KrPSbVaP3sEjSMPKII6EAeftF1-qcOgh9EngMLVYb7_gOzb1T2PFXq9mmd1SYSYdk0jfG8acQ1t__L1IqOxcV3chX3aUuERL5yWEglEOKV43_qtCihMHhU51GTr_oEuebZ7auuT6A-mVDjJdHefaT6EZx8af_307nfeik-cRij4sFpkL-ZIntP3E_wvcu9voCP-wL9fzIkQEuE32CIxGgBoNNImjZ1prqjH41Rg-IfuHLvAJpqkmiB2E5KoU9LEspXn1gpC8DC1v1cW7XfydzzcXpUOqHMuQgAa_LEy5fwcag"#;
+    
     let kid = decode_header(token)?.kid.expect("Cannot get KID");    
     if let Some(jwk) = jwks.find(&kid) {
         match jwk.algorithm {
@@ -23,6 +43,6 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             },
             _ => unreachable!("Have no implemented other alg"),
         }
-    }
+    } */
     Ok(())
 }
