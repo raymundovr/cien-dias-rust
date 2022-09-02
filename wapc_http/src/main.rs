a9e446b9a9a3e38513a30540c100aada7541aa42
Raymundo Vásquez Ruiz <raymundo.vr@protonmail.com>
2022-09-02 07:34:22 +0200

    75: WIP exponiendo wapc sobre HTTP con actix

---
+++ wapc_http/src/main.rs
@@ -0,0 +1,16 @@
+use actix_web::{App, HttpServer, Responder, HttpResponse, get};
+
+#[get("/")]
+async fn get_does_word_exist() -> impl Responder {
+    HttpResponse::Ok()
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
+}
