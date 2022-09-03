8ccf9039826a3bcfe40e9067847ccc81c14f9514
Raymundo Vásquez Ruiz <raymundo.vr@protonmail.com>
2022-09-03 08:57:57 +0200

    76: hot loading WASM

--- wapc_http/src/main.rs
+++ wapc_http/src/main.rs
@@ -1,33 +1,60 @@
-use actix_web::{App, HttpServer, get, web};
+use actix_web::{get, App, HttpResponse, HttpServer};
 use anyhow::Result;
-use serde::{Serialize};
+use serde::Serialize;
 use std::fs::read;
-use wapc::{WapcHost};
+use std::time::{Instant};
+use wapc::WapcHost;
 use wapc_codec::messagepack::{deserialize, serialize};
 
-#[derive(Debug)]
+/* #[derive(Debug)]
 struct AppState {
     pub result: bool,
 }
-
+ */
 #[derive(Serialize)]
 pub struct DoesWordExistArgs {
     pub word: String,
 }
 
 #[get("/")]
-async fn get_does_word_exist(state: web::Data<AppState>) -> String {
-    println!("{:?}", state);
-    if state.result {
-        return "yes".to_string();
-    }
+async fn get_does_word_exist() -> HttpResponse {
+    let start = Instant::now();
+    let buf = read("./wasm/cuckoo_wapc.wasm").unwrap();
+    let engine = match wasmtime_provider::WasmtimeEngineProvider::new(&buf, None) {
+        Ok(e) => e,
+        Err(_) => return HttpResponse::InternalServerError().into(),
+    };
 
-    "no".to_string()
+    let guest = WapcHost::new(
+        Box::new(engine),
+        Some(Box::new(move |_a, _b, _c, _d, _e| Ok(vec![]))),
+    )
+    .unwrap();
+
+    let call_result = guest
+        .call(
+            "does_word_exist",
+            &serialize(DoesWordExistArgs {
+                word: "esto".to_string(),
+            })
+            .unwrap(),
+        )
+        .unwrap();
+    let result: bool = deserialize(&call_result).unwrap();
+    
+    println!("Loading WASM and answering took {} milliseconds", start.elapsed().as_millis());
+
+    let answer = match result {
+        true => "yes",
+        false => "no",
+    };
+
+    HttpResponse::Ok().body(answer)
 }
 
 #[actix_web::main]
 async fn main() -> Result<()> {
-    let buf = read("./wasm/cuckoo_wapc.wasm")?;
+    /* let buf = read("./wasm/cuckoo_wapc.wasm")?;
     let engine = wasmtime_provider::WasmtimeEngineProvider::new(&buf, None)?;
     let guest = WapcHost::new(
         Box::new(engine),
@@ -36,13 +63,12 @@ async fn main() -> Result<()> {
 
     let call_result = guest.call("does_word_exist", &serialize(DoesWordExistArgs{ word: "esto".to_string() }).unwrap())?;
     let result: bool = deserialize(&call_result).unwrap();
-    
-    println!("{:?}", result);
+    println!("{:?}", result); */
 
-    HttpServer::new(move || {
+    HttpServer::new(|| {
         App::new()
-        .app_data(web::Data::new(AppState { result }))
-        .service(get_does_word_exist)
+            //.app_data(web::Data::new(AppState { result }))
+            .service(get_does_word_exist)
     })
     .bind(("127.0.0.1", 8080))?
     .run()
