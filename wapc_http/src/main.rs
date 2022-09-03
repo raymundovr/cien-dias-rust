a56f9e50d7a9f7112e8378c96fd8875181798f3f
Raymundo Vásquez Ruiz <raymundo.vr@protonmail.com>
2022-09-03 21:30:43 +0200

    76: cargando módulo WASM y clonándolo para que el endpoint lo utilice

--- wapc_http/src/main.rs
+++ wapc_http/src/main.rs
@@ -1,36 +1,31 @@
-use actix_web::{get, App, HttpResponse, HttpServer};
+use actix_web::{get, App, HttpResponse, HttpServer, web};
 use anyhow::Result;
 use serde::Serialize;
 use std::fs::read;
-use std::time::{Instant};
+use std::time::Instant;
 use wapc::WapcHost;
 use wapc_codec::messagepack::{deserialize, serialize};
+use wasmtime_provider::WasmtimeEngineProvider;
 
-/* #[derive(Debug)]
 struct AppState {
-    pub result: bool,
+    pub engine:  WasmtimeEngineProvider,
 }
- */
+
 #[derive(Serialize)]
 pub struct DoesWordExistArgs {
     pub word: String,
 }
 
 #[get("/")]
-async fn get_does_word_exist() -> HttpResponse {
+async fn get_does_word_exist(app_state: web::Data<AppState>) -> HttpResponse {
     let start = Instant::now();
-    let buf = read("./wasm/cuckoo_wapc.wasm").unwrap();
-    let engine = match wasmtime_provider::WasmtimeEngineProvider::new(&buf, None) {
-        Ok(e) => e,
-        Err(_) => return HttpResponse::InternalServerError().into(),
-    };
-
+    let engine = app_state.engine.clone();
     let guest = WapcHost::new(
         Box::new(engine),
         Some(Box::new(move |_a, _b, _c, _d, _e| Ok(vec![]))),
     )
     .unwrap();
-
+    println!("Loading guest {}", start.elapsed().as_millis());
     let call_result = guest
         .call(
             "does_word_exist",
@@ -54,20 +49,14 @@ async fn get_does_word_exist() -> HttpResponse {
 
 #[actix_web::main]
 async fn main() -> Result<()> {
-    /* let buf = read("./wasm/cuckoo_wapc.wasm")?;
-    let engine = wasmtime_provider::WasmtimeEngineProvider::new(&buf, None)?;
-    let guest = WapcHost::new(
-        Box::new(engine),
-        Some(Box::new(move |_a, _b, _c, _d, _e| Ok(vec![])))
-    )?;
+    // Después de encontrar que crear en engine es lo que más consume tiempo ahora funciona clonándolo
+    let buf = read("./wasm/cuckoo_wapc.wasm")?;
+    let engine = WasmtimeEngineProvider::new(&buf, None)?;
+    let app_state = web::Data::new(AppState { engine });
 
-    let call_result = guest.call("does_word_exist", &serialize(DoesWordExistArgs{ word: "esto".to_string() }).unwrap())?;
-    let result: bool = deserialize(&call_result).unwrap();
-    println!("{:?}", result); */
-
-    HttpServer::new(|| {
+    HttpServer::new(move || {
         App::new()
-            //.app_data(web::Data::new(AppState { result }))
+            .app_data(app_state.clone())
             .service(get_does_word_exist)
     })
     .bind(("127.0.0.1", 8080))?
