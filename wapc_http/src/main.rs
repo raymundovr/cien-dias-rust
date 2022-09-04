ab54f47300325f0fa3c26bad6096e13e16290a9d
Raymundo Vásquez Ruiz <raymundo.vr@protonmail.com>
2022-09-04 16:13:17 +0200

    77: haz que el enpoint acepte queries

--- wapc_http/src/main.rs
+++ wapc_http/src/main.rs
@@ -1,6 +1,8 @@
-use actix_web::{get, App, HttpResponse, HttpServer, web};
+use actix_web::{get, middleware::Logger, web, App, HttpResponse, HttpServer};
 use anyhow::Result;
-use serde::Serialize;
+use env_logger::Env;
+use log::info;
+use serde::{Deserialize, Serialize};
 use std::fs::read;
 use std::time::Instant;
 use wapc::WapcHost;
@@ -8,36 +10,45 @@ use wapc_codec::messagepack::{deserialize, serialize};
 use wasmtime_provider::WasmtimeEngineProvider;
 
 struct AppState {
-    pub engine:  WasmtimeEngineProvider,
+    pub engine: WasmtimeEngineProvider,
 }
 
-#[derive(Serialize)]
+#[derive(Clone, Debug, Deserialize, Serialize)]
 pub struct DoesWordExistArgs {
     pub word: String,
 }
 
-#[get("/")]
-async fn get_does_word_exist(app_state: web::Data<AppState>) -> HttpResponse {
+#[get("/does_word_exist")]
+async fn get_does_word_exist(
+    app_state: web::Data<AppState>,
+    query: web::Query<DoesWordExistArgs>,
+) -> HttpResponse {
+    
     let start = Instant::now();
     let engine = app_state.engine.clone();
+    info!("Query string {:?}", query);
+    let word = &query.word;
     let guest = WapcHost::new(
         Box::new(engine),
         Some(Box::new(move |_a, _b, _c, _d, _e| Ok(vec![]))),
     )
     .unwrap();
-    println!("Loading guest {}", start.elapsed().as_millis());
+    info!("Loading guest {}", start.elapsed().as_millis());
     let call_result = guest
         .call(
             "does_word_exist",
             &serialize(DoesWordExistArgs {
-                word: "esto".to_string(),
+                word: word.to_string(),
             })
             .unwrap(),
         )
         .unwrap();
     let result: bool = deserialize(&call_result).unwrap();
-    
-    println!("Loading WASM and answering took {} milliseconds", start.elapsed().as_millis());
+
+    info!(
+        "Loading WASM and answering took {} milliseconds",
+        start.elapsed().as_millis()
+    );
 
     let answer = match result {
         true => "yes",
@@ -53,10 +64,13 @@ async fn main() -> Result<()> {
     let buf = read("./wasm/cuckoo_wapc.wasm")?;
     let engine = WasmtimeEngineProvider::new(&buf, None)?;
     let app_state = web::Data::new(AppState { engine });
+    env_logger::init_from_env(Env::default().default_filter_or("info"));
 
     HttpServer::new(move || {
         App::new()
             .app_data(app_state.clone())
+            .wrap(Logger::default())
+            .wrap(Logger::new("%a %{User-Agent}i"))
             .service(get_does_word_exist)
     })
     .bind(("127.0.0.1", 8080))?
