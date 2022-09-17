587f9f1d3376da2ff455ee52aab1c5b15d06b64b
Raymundo Vásquez Ruiz <raymundo.vr@protonmail.com>
2022-09-17 21:51:40 +0200

    85: Qs de correlación

--- request-reply-arch/common-q/src/lib.rs
+++ request-reply-arch/common-q/src/lib.rs
@@ -1,10 +1,10 @@
 /// Common functionalities for Qs
-use amiquip::{Channel, Connection, Exchange, Publish, Result};
+use amiquip::{Channel, Connection, ConsumerMessage, ConsumerOptions, Exchange, Publish, Result, QueueDeclareOptions};
 
 pub struct Queue<'a> {
-    pub connection: Connection,
-    pub channel: Channel,
-    pub name: &'a str,
+    connection: Connection,
+    channel: Channel,
+    name: &'a str,
 }
 
 impl<'a> Queue<'a> {
@@ -22,8 +22,27 @@ impl<'a> Queue<'a> {
         self.connection.close()
     }
 
-    pub fn publish_message(self, message: &[u8]) -> Result<()> {
+    pub fn publish_message(&self, message: &[u8]) -> Result<()> {
         let exchange = Exchange::direct(&self.channel);
         exchange.publish(Publish::new(message, self.name))
+    }
+
+    pub fn read_queue(&self) -> Result<(), amiquip::Error> {
+        let queue = self.channel.queue_declare(self.name, QueueDeclareOptions::default())?;
+        let consumer = queue.consume(ConsumerOptions::default())?;
+
+        for (i, message) in consumer.receiver().iter().enumerate() {
+            match message {
+                ConsumerMessage::Delivery(delivery) => {
+                    let body = String::from_utf8_lossy(&delivery.body);
+                    println!("[{}] I did receive {}", i, body);
+                },
+                other => {
+                    println!("Consumer Q ended {:?}", other);
+                }
+            }
+        }
+        
+        Ok(())
     }
 }
--- request-reply-arch/correlation-id/src/main.rs
+++ request-reply-arch/correlation-id/src/main.rs
@@ -1,4 +1,34 @@
+mod producer;
+use clap::{Parser, ValueEnum};
+
+const URL: &'static str = "amqp://guest:guest@localhost:5672";
+
+#[derive(Clone, PartialEq, Eq, ValueEnum)]
+enum Mode {
+    Consumer,
+    Producer,
+}
+
+#[derive(Parser)]
+struct Args {
+    #[clap(arg_enum, value_parser)]
+    mode: Mode,
+    #[clap(short, long, value_parser)]
+    id: String,
+    #[clap(short, long, value_parser)]
+    queue: String,
+}
 
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    match args.mode {
+        Mode::Consumer => {
+            println!("Running as consumer. Instance ID {}", args.id);
+        }
+        Mode::Producer => {
+            println!("Running as producer. Instance ID {}", args.id);
+            producer::init(URL, &args.id, &args.queue);
+        }
+    }
 }
